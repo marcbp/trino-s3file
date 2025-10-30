4f1763c54eaff0f0cc8a2f8d3a9dcd81df50acf9
Marc Blanc-Patin <marc.blancpatin@bpmconcept.com>
2025-10-30 23:27:02 +0100

    Use replicas for docker compose

--- docker/etc/worker/entrypoint.sh
+++ docker/etc/worker/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ -z "${TRINO_NODE_ID:-}" ]]; then
-  echo "TRINO_NODE_ID environment variable must be set for worker nodes" >&2
+NODE_ID="${TRINO_NODE_ID:-${HOSTNAME:-}}"
+if [[ -z "${NODE_ID}" ]]; then
+  echo "Unable to determine node ID; set TRINO_NODE_ID or ensure HOSTNAME is available" >&2
   exit 1
 fi
 
 NODE_CONFIG=/tmp/node.properties
-sed "s/{{NODE_ID}}/${TRINO_NODE_ID}/" /etc/trino-worker/node.properties.tpl > "${NODE_CONFIG}"
+sed "s/{{NODE_ID}}/${NODE_ID}/" /etc/trino-worker/node.properties.tpl > "${NODE_CONFIG}"
 
 exec /usr/lib/trino/bin/run-trino \
   --config /etc/trino-worker/config.properties \
